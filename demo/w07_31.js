f11fa53e7629a783f1d9e1455dce9fd91ba1f646
hang0727 <213417131@o365.tku.edu.tw>
2024-10-25 20:27:32 +0800

    w07-p4: Create navber with links to class demo w02, w03, w06, and deploy it to Vercel

--- demo/w07_31.js
+++ demo/w07_31.js
@@ -1,1 +1,16 @@
-alert('hello')
+const p = document.querySelector('.show-classdemo')
+console.log('show-classdemo', p)
+
+function showDemo(item) {
+    switch(item) {
+        case 'w02_intro_31' :
+            p.innerHTML = `<iframe src='./w02_intro_31/index.html' width="100%" height="100%" />`
+            break;
+        case 'w03_card_31' :
+             p.innerHTML = `<iframe src='./w03_card_31/' width="100%" height="100%" />`
+            break;
+        case 'w06_blog_31' :
+            p.innerHTML = `<iframe src='./w06_blog_31/' width="100%" height="100%" />`
+            break;
+    }
+}
